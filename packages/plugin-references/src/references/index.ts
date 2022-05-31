0281f56dab0f8b1d8ff15e1de3a9af010f2286cd
jrtnq514 <burggamer.jt@gmail.com>
2022-05-30 20:38:51 -0500

    checking stuff in

--- packages/plugin-references/src/references/index.ts
+++ packages/plugin-references/src/references/index.ts
@@ -7,13 +7,23 @@ interface InitReferencesProps {
   configPath?: string;
 }
 
-export const initReferences = ({configPath = './references.config.js'}: InitReferencesProps): ReferencesPlugin => {
-  console.log(configPath)
+export const initReferences = ({configPath = '.'}: InitReferencesProps): ReferencesPlugin => {
   return {
     packageName: '@astral-dx/plugin-references',
     getReferences: async () => {
+      const FILE_NAME = 'references.config.js';
 
-      console.log(fs.lstatSync(configPath))
+      try {
+        console.log(`${configPath}/${FILE_NAME}`);
+        console.log(fs.readdirSync(configPath));
+        // const config = await fs.readFile(`${configPath}/${FILE_NAME}`)
+        // console.log(config)
+        // console.log(Buffer.from(config).toJSON());
+        const config = await import(`${configPath}/${FILE_NAME}`);
+        console.log(config);
+      } catch (e) {
+        throw new Error(`Could not load references config at ${configPath}/${FILE_NAME}`);
+      }
 
       return [
         { url: 'https://api.nasa.gov/', label: 'Documentation', description: 'This catalog focuses on broadly useful and user friendly APIs.', icon: 'book' },
